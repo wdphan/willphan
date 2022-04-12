941c9f38111474b2ada2a459182cfe683a375c66
gareth-was-here <gareth@garethveale.dev>
2022-04-12 10:11:45 +0100

    move header to component file + install threejs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import './App.css';
+import Header from './Header.js';
 
 function App() {
   return (
-    <div className="index-wrapper">
-      <h1 className="home-title">gareth</h1>
-      <div style={{ display : `flex` }}>
-        <div className="css-typing">
-          <p className="index-1">developer/builder... web3 / blockchain / 3d </p>
-          <p className="index-2">previously @<a style={{ color: `#44e407` }} href="https://www.jpmorgan.com/global" target="_blank" rel="noopener noreferrer">JPMorgan</a> 4 years, currently @
-            <a style={{ color: `#44e407`  }} href="https://aave.com/" target="_blank" rel="noopener noreferrer">Aave</a>   
-          </p>
-          <p className="index-3">> see my <a style={{ color: `#44e407` }} href="https://github.com/gareth-xyz" target="_blank" rel="noopener noreferrer"> projects on github </a></p>
-          <p className="index-4">
-            <a style={{ color: `#44e407` }} href="https://twitter.com/gareth_xyz" target="_blank" rel="noopener noreferrer">twitter </a> /  
-            <a style={{ color: `#44e407` }} href="mailto:gareth@garethv.xyz" target="_blank" rel="noopener noreferrer"> email</a> / 
-            <a style={{ color: `#44e407` }} href="https://github.com/gareth-xyz/3d-site" target="_blank" rel="noopener noreferrer"> this site</a>
-          </p>
-        </div>
-      </div>
-    </div>
+    <Header />
   );
 }
 
