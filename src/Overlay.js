1bcf2a2db91e6ab1a820b5bf328bce65b434c4c2
gareth-was-here <gareth@garethveale.dev>
2022-04-22 23:56:56 +0200

    remove typing animation and change overlay

--- src/Overlay.js
+++ src/Overlay.js
@@ -5,15 +5,15 @@ function Overlay() {
       <h1 className="home-title">gareth</h1>
       <div style={{ display : `flex` }}>
         <div className="css-typing">
-          <p className="index-1">developer/builder... web3 / blockchain / 3d </p>
-          <p className="index-2">previously @<a style={{ color: `#44e407` }} href="https://www.jpmorgan.com/global" target="_blank" rel="noopener noreferrer">JPMorgan</a> 4 years, currently @
+          <p className="index-1">blockchain / web3 developer</p>
+          <p className="index-2">previously @<a style={{ color: `#44e407` }} href="https://www.jpmorgan.com/global" target="_blank" rel="noopener noreferrer">JPMorgan</a>, currently @
             <a style={{ color: `#44e407`  }} href="https://aave.com/" target="_blank" rel="noopener noreferrer">Aave</a>   
           </p>
-          <p className="index-3">> see my <a style={{ color: `#44e407` }} href="https://github.com/gareth-xyz" target="_blank" rel="noopener noreferrer"> projects on github </a></p>
+          <p className="index-3">><a style={{ color: `#44e407` }} href="https://garethv.notion.site/Notes-c38d4b5b2cf944f0899244266e2fa3a9" target="_blank" rel="noopener noreferrer"> writing</a></p>
+          <p className="index-3">><a style={{ color: `#44e407` }} href="https://github.com/gareth-xyz" target="_blank" rel="noopener noreferrer"> projects</a></p>
           <p className="index-4">
-            <a style={{ color: `#44e407` }} href="https://twitter.com/gareth_xyz" target="_blank" rel="noopener noreferrer">twitter</a> /  
-            <a style={{ color: `#44e407` }} href="mailto:gareth@garethv.xyz" target="_blank" rel="noopener noreferrer"> email </a> / 
-            <a style={{ color: `#44e407` }} href="https://garethv.notion.site/Notes-c38d4b5b2cf944f0899244266e2fa3a9" target="_blank" rel="noopener noreferrer"> writing</a>
+            <a style={{ color: `#44e407`, textDecoration: `None` }} href="https://twitter.com/gareth_xyz" target="_blank" rel="noopener noreferrer">twitter </a>/  
+            <a style={{ color: `#44e407`, textDecoration: `None` }} href="mailto:gareth@garethv.xyz" target="_blank" rel="noopener noreferrer"> email</a>
           </p>
         </div>
       </div>
