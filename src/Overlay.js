937d33b8798f4eb61471cda536b6602889a0bf76
gareth-was-here <gareth@garethveale.dev>
2022-04-27 10:48:13 +0100

    change link

--- src/Overlay.js
+++ src/Overlay.js
@@ -9,7 +9,7 @@ function Overlay() {
           <p className="index-2">@<a style={{ color: `#44e407`  }} href="https://aave.com/" target="_blank" rel="noopener noreferrer">Aave</a>, previously <a style={{ color: `#44e407` }} href="https://www.jpmorgan.com/global" target="_blank" rel="noopener noreferrer">JPMorgan</a>
           </p>
           <p className="index-3">><a style={{ color: `#44e407` }} href="https://garethv.notion.site/Notes-c38d4b5b2cf944f0899244266e2fa3a9" target="_blank" rel="noopener noreferrer"> notes</a></p>
-          <p className="index-3">><a style={{ color: `#44e407` }} href="https://github.com/0x-gareth" target="_blank" rel="noopener noreferrer"> projects</a></p>
+          <p className="index-3">><a style={{ color: `#44e407` }} href="https://github.com/0xgareth" target="_blank" rel="noopener noreferrer"> projects</a></p>
           <p className="index-4">
             <a style={{ color: `#44e407`, textDecoration: `None` }} href="https://twitter.com/0x_gareth" target="_blank" rel="noopener noreferrer">twitter </a>/  
             <a style={{ color: `#44e407`, textDecoration: `None` }} href="mailto:gareth@garethv.xyz" target="_blank" rel="noopener noreferrer"> email</a>
